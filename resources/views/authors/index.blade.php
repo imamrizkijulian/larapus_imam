ecf7286f636c5e81c6ceed9eaaa2b752fba6f4e5
imamrizkijulian <151610211@smkassalaambandung.sch.id>
2017-08-04 23:05:41 +0700

    Crud Authors Beres

---
+++ resources/views/authors/index.blade.php
@@ -0,0 +1,26 @@
+@extends('layouts.app')
+@section('content')
+	<div class="container">
+		<div class="row">
+			<div class="col-md-12">
+				<ul class="breadcrumb">
+					<li><a href="{{ url('/home') }}">Dashboard</a></li>
+					<li class="active">Penulis</li>
+				</ul>
+
+				<div class="panel panel-primary">
+					<div class="panel-heading">
+						<button class="btn btn-default"><a href="{{ route('authors.create') }}">Tambah Data</a></button>
+					</div>
+					<div class="panel-body">
+						{!! $html->table(['class'=>'table-striped']) !!}
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+@endsection
+
+@section('scripts')
+{!! $html->scripts() !!}
+@endsection
